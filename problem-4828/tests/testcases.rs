ffd88c107994b0e07a7635ce43d0b49ecb23fb0c
Danuel <public.danuel@gmail.com>
2022-03-08 15:20:22 +0900

    migrate convention

--- problem-4828/tests/testcases.rs
+++ problem-4828/tests/testcases.rs
@@ -2,40 +2,40 @@ use problem_4828::solve;
 
 #[test]
 fn example() {
-    let source = include_str!("fixtures/example/xml.in").trim();
+    let source = include_str!("fixtures/example/in").trim();
 
     assert_eq!(
         solve(source),
-        Ok(include_str!("fixtures/example/xml.out").trim().to_string())
+        Ok(include_str!("fixtures/example/out").trim().to_string())
     );
 }
 
 #[test]
 fn test1() {
-    let source = include_str!("fixtures/test1/xml.in").trim();
+    let source = include_str!("fixtures/test1/in").trim();
 
     assert_eq!(
         solve(source),
-        Ok(include_str!("fixtures/test1/xml.out").trim().to_string())
+        Ok(include_str!("fixtures/test1/out").trim().to_string())
     );
 }
 
 #[test]
 fn test2() {
-    let source = include_str!("fixtures/test2/xml.in").trim();
+    let source = include_str!("fixtures/test2/in").trim();
 
     assert_eq!(
         solve(source),
-        Ok(include_str!("fixtures/test2/xml.out").trim().to_string())
+        Ok(include_str!("fixtures/test2/out").trim().to_string())
     );
 }
 
 #[test]
 fn test3() {
-    let source = include_str!("fixtures/test3/xml.in").trim();
+    let source = include_str!("fixtures/test3/in").trim();
 
     assert_eq!(
         solve(source),
-        Ok(include_str!("fixtures/test3/xml.out").trim().to_string())
+        Ok(include_str!("fixtures/test3/out").trim().to_string())
     );
 }
