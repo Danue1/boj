994848ccf5131e52e35942aa3d706057e6b3482f
Danuel <public.danuel@gmail.com>
2022-03-09 02:24:13 +0900

    BOJ 8393

---
+++ problem-8393/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    use std::io::Read;
+
+    let mut io = std::io::stdin();
+    let mut input = String::new();
+    match io.read_to_string(&mut input) {
+        Ok(_) => match solve(&input) {
+            Ok(output) => print!("{}", output),
+            Err(error) => print!("{:#?}", error),
+        }
+        Err(error) => print!("{:#?}", error),
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    Fmt(std::fmt::Error),
+    Int(std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    let mut output = String::new();
+    for line in input.lines() {
+        let max = line.parse()?;
+        let num: u32 = (1..=max).sum();
+        writeln!(output, "{}", num)?;
+    }
+    Ok(output)
+}
+
+impl From<std::fmt::Error> for Error {
+    fn from(error: std::fmt::Error) -> Self {
+        Error::Fmt(error)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Error::Int(error)
+    }
+}
