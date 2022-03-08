46b25b1a152245a44033a4f03b4a18d16bcb35eb
Danuel <public.danuel@gmail.com>
2022-03-09 02:15:01 +0900

    BOJ 14490

---
+++ problem-14490/src/main.rs
@@ -0,0 +1,52 @@
+fn main() {
+    use std::io::Read;
+
+    let mut io = std::io::stdin();
+    let mut input = String::new();
+    match io.read_to_string(&mut input) {
+        Ok(_) => match solve(&input) {
+            Ok(output) => print!("{}", output),
+            Err(error) => print!("{:#?}", error),
+        }
+        Err(error) => print!("{:#?}", error),
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    Fmt(std::fmt::Error),
+    Int(std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    let mut output = String::new();
+    for line in input.lines() {
+        let mut values = line.split(':');
+        let m: u32 = values.next().unwrap().parse()?;
+        let n: u32 = values.next().unwrap().parse()?;
+        let gcd = gcd(m, n);
+        writeln!(output, "{}:{}", m / gcd, n / gcd)?;
+    }
+    Ok(output)
+}
+
+fn gcd(m: u32, n: u32) -> u32 {
+    match m {
+        0 => n,
+        _ => gcd(n % m, m),
+    }
+}
+
+impl From<std::fmt::Error> for Error {
+    fn from(error: std::fmt::Error) -> Self {
+        Error::Fmt(error)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Error::Int(error)
+    }
+}
