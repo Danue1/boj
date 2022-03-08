ad5026a142a9717b60b1a612d795c1a45c4f658f
Danuel <public.danuel@gmail.com>
2022-03-08 15:42:02 +0900

    BOJ 10951

---
+++ problem-10951/src/lib.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    Fmt(std::fmt::Error),
+    Int(std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    let mut output = String::new();
+    for line in input.lines() {
+        let mut tokens = line.split_whitespace();
+        let a: i32 = tokens.next().unwrap().parse()?;
+        let b: i32 = tokens.next().unwrap().parse()?;
+        writeln!(output, "{}", a + b)?;
+    }
+    Ok(output)
+}
+
+impl From<std::fmt::Error> for Error {
+    fn from(error: std::fmt::Error) -> Self {
+        Error::Fmt(error)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Error::Int(error)
+    }
+}
