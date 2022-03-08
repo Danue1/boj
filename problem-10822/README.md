75e773dac24d185777e271b20d79c51b7c75c9af
Danuel <public.danuel@gmail.com>
2022-03-09 01:45:03 +0900

    BOJ 10822

---
+++ problem-10822/README.md
@@ -0,0 +1,1 @@
+[Reference](https://www.acmicpc.net/problem/10822)
---
+++ problem-10822/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    Fmt(std::fmt::Error),
+    Int(std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    let mut output = String::new();
+    for line in input.lines() {
+        let mut num = 0;
+        for value in line.split(',') {
+            let value: u32 = value.parse()?;
+            num += value;
+        }
+        writeln!(output, "{}", num)?;
+    }
+    Ok(output)
+}
+
+impl From<std::fmt::Error> for Error {
+    fn from(error: std::fmt::Error) -> Self {
+        Error::Fmt(error)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Error::Int(error)
+    }
+}
