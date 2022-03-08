a66179d3ec695d277277e3b8317a0018b2071f01
Danuel <public.danuel@gmail.com>
2022-03-08 15:32:54 +0900

    BOJ 2558

---
+++ problem-2558/src/lib.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    Fmt(std::fmt::Error),
+    Int(std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    let mut output = String::new();
+    let mut num: i32 = 0;
+    for token in input.split_whitespace() {
+        let token: i32 = token.parse()?;
+        num += token;
+    }
+    writeln!(output, "{}", num)?;
+    Ok(output)
+}
+
+impl From<std::fmt::Error> for Error {
+    fn from(error: std::fmt::Error) -> Self {
+        Error::Fmt(error)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Error::Int(error)
+    }
+}
---
+++ problem-2558/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    use std::io::Read;
+
+    let mut io = std::io::stdin();
+    let mut input = String::new();
+    match io.read_to_string(&mut input) {
+        Ok(_) => match solve(&input) {
+            Ok(output) => print!("{}", output),
+            Err(error) => print!("{:#?}", error),
+        }
+        Err(error) => print!("{:#?}", error),
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    Fmt(std::fmt::Error),
+    Int(std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    let mut output = String::new();
+    let mut num: i32 = 0;
+    for token in input.split_whitespace() {
+        let token: i32 = token.parse()?;
+        num += token;
+    }
+    writeln!(output, "{}", num);
+    Ok(output)
+}
+
+impl From<std::fmt::Error> for Error {
+    fn from(error: std::fmt::Error) -> Self {
+        Error::Fmt(error)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Error::Int(error)
+    }
+}
