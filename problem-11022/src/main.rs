1aceab518711a70650988076303526b5defcd0d7
Danuel <public.danuel@gmail.com>
2022-03-08 16:03:25 +0900

    BOJ 11022

---
+++ problem-11022/src/main.rs
@@ -0,0 +1,46 @@
+fn main() {
+    use std::io::Read;
+
+    let mut io = std::io::stdin();
+    let mut input = String::new();
+    match io.read_to_string(&mut input) {
+        Ok(_) => match solve(&input) {
+            Ok(output) => print!("{}", output),
+            Err(error) => print!("{:#?}", error),
+        }
+        Err(error) => print!("{:#?}", error),
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    Fmt(std::fmt::Error),
+    Int(std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    let mut output = String::new();
+    let mut lines = input.lines().enumerate();
+    lines.next();
+    for (index, line) in lines {
+        let mut tokens = line.split_ascii_whitespace();
+        let a: i32 = tokens.next().unwrap().parse()?;
+        let b: i32 = tokens.next().unwrap().parse()?;
+        writeln!(output, "Case #{}: {} + {} = {}", index, a, b, a + b)?;
+    }
+    Ok(output)
+}
+
+impl From<std::fmt::Error> for Error {
+    fn from(error: std::fmt::Error) -> Self {
+        Error::Fmt(error)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Error::Int(error)
+    }
+}
