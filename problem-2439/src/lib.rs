a45d14a8d22079afc7bca6bb9b865bd35d627c02
Danuel <public.danuel@gmail.com>
2022-03-09 03:16:40 +0900

    BOJ 2439

---
+++ problem-2439/src/lib.rs
@@ -0,0 +1,36 @@
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    Fmt(std::fmt::Error),
+    Int(std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    let mut output = String::new();
+    for line in input.lines() {
+        let max = line.parse()?;
+        for index in 1..=max {
+            for _ in 0..(max - index) {
+                write!(output, " ")?;
+            }
+            for _ in 0..index {
+                write!(output, "*")?;
+            }
+            writeln!(output)?;
+        }
+    }
+    Ok(output)
+}
+
+impl From<std::fmt::Error> for Error {
+    fn from(error: std::fmt::Error) -> Self {
+        Error::Fmt(error)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Error::Int(error)
+    }
+}
