bdf9a5d3bf738642c083c98c44d8b4e51cfe470d
Danuel <public.danuel@gmail.com>
2022-03-10 15:37:32 +0900

    BOJ 2870

---
+++ problem-2xxx/problem-2870/src/lib.rs
@@ -0,0 +1,51 @@
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    Fmt(std::fmt::Error),
+    Int(std::num::ParseIntError),
+}
+
+pub fn solve(input: &str) -> Result<String, Error> {
+    use std::fmt::Write;
+
+    let mut output = String::new();
+    let mut lines = input.lines();
+    let max = lines.next().unwrap().parse()?;
+    let mut nums: Vec<&str> = lines
+        .take(max)
+        .flat_map(|line| line.split(|c| !matches!(c, '0'..='9')))
+        .filter_map(|num| {
+            if !num.is_empty() {
+                if let Some(index) = num.find(|c| matches!(c, '1'..='9')) {
+                    Some(&num[index..])
+                } else {
+                    Some("0")
+                }
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    nums.sort_by(|a, b| match a.len().cmp(&b.len()) {
+        std::cmp::Ordering::Equal => a.cmp(b),
+        other => other,
+    });
+
+    for num in nums {
+        writeln!(output, "{}", num)?;
+    }
+
+    Ok(output)
+}
+
+impl From<std::fmt::Error> for Error {
+    fn from(error: std::fmt::Error) -> Self {
+        Error::Fmt(error)
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Error::Int(error)
+    }
+}
